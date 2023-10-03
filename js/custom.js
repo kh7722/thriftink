315a217d7d1efa48cf848e22a7efc8d5804ef60e
Kyleigh Teeters <khess95@hotmail.com>
2023-10-03 19:52:21 -0400

    style updates and slider

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,32 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+const carousel = document.querySelector('.carousel');
+const prevBtn = document.querySelector('.prev-slide');
+const nextBtn = document.querySelector('.next-slide');
+const slides = document.querySelectorAll('.slide');
+let currentIndex = 0;
+
+nextBtn.addEventListener('click', () => {
+    if (currentIndex < slides.length - 1) {
+        currentIndex++;
+        updateCarousel();
+    }
+});
+
+prevBtn.addEventListener('click', () => {
+    if (currentIndex > 0) {
+        currentIndex--;
+        updateCarousel();
+    }
+});
+
+function updateCarousel() {
+    const translateX = -currentIndex * 100;
+    carousel.style.transition = 'transform 0.3s ease-in-out';
+    carousel.style.transform = `translateX(${translateX}%)`;
+}
+
+// Initial update
+updateCarousel();
