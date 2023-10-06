ea57e04d8ea82ad5b1f59180481f7146f32d664b
Kyleigh Teeters <khess95@hotmail.com>
2023-10-05 20:04:38 -0400

    content-block-2 updates

--- js/custom.js
+++ js/custom.js
@@ -7,31 +7,3 @@ function myFunction() {
   }
 }
 
-const carousel = document.querySelector('.carousel');
-const prevBtn = document.querySelector('.prev-slide');
-const nextBtn = document.querySelector('.next-slide');
-const slides = document.querySelectorAll('.slide');
-let currentIndex = 0;
-
-nextBtn.addEventListener('click', () => {
-    if (currentIndex < slides.length - 1) {
-        currentIndex++;
-        updateCarousel();
-    }
-});
-
-prevBtn.addEventListener('click', () => {
-    if (currentIndex > 0) {
-        currentIndex--;
-        updateCarousel();
-    }
-});
-
-function updateCarousel() {
-    const translateX = -currentIndex * 100;
-    carousel.style.transition = 'transform 0.3s ease-in-out';
-    carousel.style.transform = `translateX(${translateX}%)`;
-}
-
-// Initial update
-updateCarousel();
