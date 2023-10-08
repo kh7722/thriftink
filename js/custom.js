1f576183f4b08e867379ea8024a0eb47e4bbc676
Kyleigh Teeters <khess95@hotmail.com>
2023-10-08 19:48:02 -0400

    troubleshooting nav bar

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,21 @@
 function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
   } else {
-    x.className = "topnav";
+    x.style.display = "block";
   }
 }
+
+
+
+//function myFunction() {
+ // var x = document.getElementById("myTopnav");
+//  if (x.className === "topnav") {
+//    x.className += " responsive";
+//  } else {
+//    x.className = "topnav";
+//  }
+//}
+
 
