f81d1537fef5191dbec3950f40699ecd061729f5
Kyleigh Teeters <khess95@hotmail.com>
2023-10-24 20:54:56 -0400

    nav edits

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,10 @@
 function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
   } else {
-    x.style.display = "block";
+    x.className = "topnav";
   }
 }
-
-
-
-//function myFunction() {
- // var x = document.getElementById("myTopnav");
-//  if (x.className === "topnav") {
-//    x.className += " responsive";
-//  } else {
-//    x.className = "topnav";
-//  }
-//}
 
 
